d42ff09ac1ed5ff746707417b1d0504c901cb9f6
sachinrana396 <ranasachin396@gmail.com>
2023-01-12 20:46:38 +0530

    add startup page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import matplotlib.ticker as mticker
 import pandas as pd
 import matplotlib.pyplot as plt
 
 st.set_page_config(layout='wide',page_title='Startup Funding')
 
-df = pd.read_csv('startup_funding.csv')
-df = df.drop(columns='Unnamed: 0')
+df = pd.read_csv('Stat6.csv')
 df = df.set_index('Sr No')
 # st.dataframe(df)
 df['Date'] = pd.to_datetime(df.Date,errors='coerce')
@@ -50,16 +50,21 @@ def load_overall_analysis():
     if selected_option =='Total Startup Funded':
         fig, ax = plt.subplots()
         ax.plot(temp_df_count['x_axis'],temp_df_count['Startup'])
+        myLocator = mticker.MultipleLocator(4)
+        ax.xaxis.set_major_locator(myLocator)
+        plt.xticks(rotation='vertical')
         st.pyplot(fig)
     else:
         
         fig, ax = plt.subplots()
         ax.plot(temp_df_amount['x_axis'],temp_df_amount['Amount'])
+        myLocator = mticker.MultipleLocator(4)
+        ax.xaxis.set_major_locator(myLocator)
+        plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
 
 
-round(df.Amount.sum())
 # Investor page 
 def load_investor_details(investor):
     st.title(investor)
@@ -83,6 +88,7 @@ def load_investor_details(investor):
 
         st.pyplot(fig)
     col3, col4 = st.columns(2)
+    
     # Investment Type
     with col3:
         st.subheader('Most Invested Sectors')
@@ -107,25 +113,95 @@ def load_investor_details(investor):
         df['Year'] = df['Date'].dt.year
 
         st.subheader('YoY Investments')
-        investment_series = df[df.Investors.str.contains(investor)].groupby('Year')['Amount'].sum().sort_values(ascending=False)
+        investor_series =df[df.Investors.str.contains(name)].groupby('Year')['Amount'].sum().reset_index()
         fig, ax = plt.subplots()
-        ax.plot(investment_series.index,investment_series.values)
+        ax.plot(investor_series.Year, investor_series.Amount)
 
         st.pyplot(fig)
-    
+
+def load_startup_details(name):
+    st.title(name)
+    col1, col2, col3, col4= st.columns(4)
+
+    # Industry Vertical
+    with col1:
+        st.metric(label='Industry Vertical',value=df[df['Startup']==name]['Vertical'].iloc[0])
+
+    # Industry SubVertical
+    with col2:
+        st.metric(label='Industry SubVertical',value=df[df['Startup']==name]['SubVertical'].iloc[0])
+
+    # Location
+    with col3:
+        st.metric(label='Location',value=df[df['Startup']==name]['City'].iloc[0])
+
+    # Total Funding
+    with col4:
+        st.metric(label='Total Funding',value=df.groupby('Startup')['Amount'].sum().loc[name])
+
+    # Funding Rounds
+    st.subheader('Funding Rounds')
+    st.dataframe(df[df['Startup']==name][['Date','Type','Investors']])
+
+    # YoY Funding
+    st.subheader('YOY Funding')
+    new_df = df[['Startup','Date','Amount']]
+    new_df = new_df.sort_values(by='Date')
+    fig,ax = plt.subplots()
+    ax.plot(new_df[new_df['Startup'] == name].Date,new_df[new_df['Startup'] == name].Amount)
+    st.pyplot(fig)
+
+    # Similar Startups
+    st.subheader('Similar Startups')
+    mask1,mask2 = df['Vertical'].values, df['SubVertical'].values
+    mask3, mask4 = (df[df['Startup']==name]['Vertical']).values[0], (df[df['Startup']==name]['SubVertical']).values[0] 
+    temp_df = df[(mask1 == mask3)|(mask2==mask4)][df['Startup']!=name]['Startup']
+    col5,col6, col7 = st.columns(3)
+    with col5:
+        try:
+            st.write(f"#### {temp_df.iloc[0]}", markdown=True)
+        except:
+            pass
+    with col6:
+        try:
+            st.write(f"#### {temp_df.iloc[1]}", markdown=True)
+        except:
+            pass
+
+    with col7:
+        try:
+            st.write(f"#### {temp_df.iloc[2]}", markdown=True)
+        except:
+            pass
+
+
+
+
 
 
     
 if option == 'Overall Analysis':
-    st.title('Overall Analysis')
-    load_overall_analysis()
+    try:
+        st.title('Overall Analysis')
+        load_overall_analysis()
+    except:
+        pass
 
 elif option =='Startup':
-    st.sidebar.selectbox('Select Startup',df['Startup'].drop_duplicates().to_list())
+    name = st.sidebar.selectbox('Select Startup',df['Startup'].drop_duplicates().to_list())
     btn1 = st.sidebar.button('Find Startup Details')
+    if btn1:
+        load_startup_details(name)
 
 else:
-    name = st.sidebar.selectbox('Select Investor',sorted(set(df.Investors.str.split(',').sum())))
-    btn2 = st.sidebar.button('Find Investor Details')
-    if btn2:
-        load_investor_details(name)
+    try:
+        name = st.sidebar.selectbox('Select Investor',sorted(set(df.Investors.str.split(',').sum())))
+        btn2 = st.sidebar.button('Find Investor Details')
+        if btn2:
+            load_investor_details(name)
+    except:
+        pass
+
+# df.loc[df[df['Startup'] == 'https://www.wealthbucket.in/'].index.values,'Startup'] = ['Wealth Bucket']
+# df.to_csv('n_startup.csv')
+# print(df.loc[df[df['Startup'] == 'https://www.wealthbucket.in/'].index.values,'Startup'])
